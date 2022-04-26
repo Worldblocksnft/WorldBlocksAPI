1fc3fca716ea992e7742fff697e789c0cfec4a9a
Charlie H <91059351+charlieoriginal@users.noreply.github.com>
2022-04-26 15:43:40 +0100

    Item Serialization & NBT Editing
    + NBTEditor.java
    + SerializationUtils.java (Serialize/Deserialize Items)

--- src/main/java/net/worldblocks/libs/worldblocksapi/WorldBlocksAPI.java
+++ src/main/java/net/worldblocks/libs/worldblocksapi/WorldBlocksAPI.java
@@ -1,32 +1,58 @@
 package net.worldblocks.libs.worldblocksapi;
 
 import net.worldblocks.libs.worldblocksapi.databases.DatabaseModule;
+import net.worldblocks.libs.worldblocksapi.utilities.NBTEditor;
+import net.worldblocks.libs.worldblocksapi.utilities.SerializationUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public final class WorldBlocksAPI extends JavaPlugin {
 
-    private Map<String, Module> modules = new HashMap<>();
+    private Map<Modules, Module> modules = new HashMap<>();
 
     @Override
     public void onEnable() {
         instantiateDefaultModules();
+
+        ItemStack itemStack = new ItemStack(Material.BOOK);
+        itemStack.setAmount(45);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.addEnchant(Enchantment.ARROW_DAMAGE, 1, true);
+        itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        List<String> lore = new ArrayList<>();
+        lore.add("Epic Memes");
+        itemMeta.setLore(lore);
+        itemMeta.setDisplayName("Epic Memes #2");
+        itemStack.setItemMeta(itemMeta);
+        itemStack = NBTEditor.set(itemStack, 2, "awesome!");
+
+        System.out.println(SerializationUtils.serializeItem(itemStack));
+
+        ItemStack deserialized = SerializationUtils.deserializeItem(SerializationUtils.serializeItem(itemStack));
+        System.out.println(deserialized);
     }
 
-    public Module getModuleGeneric(String id) {
+    public Module getModuleGeneric(Modules id) {
         return modules.get(id);
     }
 
-    public void instantiateModule(String name, Module module) {
+    public void instantiateModule(Modules id, Module module) {
         module.instantiate(this);
-        modules.put(name, module);
+        modules.put(id, module);
     }
 
     public void instantiateDefaultModules() {
-        instantiateModule("database", new DatabaseModule());
+        instantiateModule(Modules.DATABASES, new DatabaseModule());
     }
 
     public static WorldBlocksAPI getAPI() {
